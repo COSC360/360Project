33cc5bfda79e19a75044f0b9ad86ed24d31c427b
WilfredGitGud <wilfredmeyer1@gmail.com>
2023-03-26 22:26:56 -0700

    Udpated db username/password

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php   
 $DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
+$DATABASE_USER = '41797044';
+$DATABASE_PASS = '41797044';
 $DATABASE_NAME = 'prompthub';
 
 $conn = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
