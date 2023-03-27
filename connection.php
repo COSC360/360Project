09c613f74748e4174d232ec8a56f08d46e06ea5b
WilfredGitGud <wilfredmeyer1@gmail.com>
2023-03-26 22:28:58 -0700

    Updated db name

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = '41797044';
 $DATABASE_PASS = '41797044';
-$DATABASE_NAME = 'prompthub';
+$DATABASE_NAME = 'db_41797044';
 
 $conn = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
